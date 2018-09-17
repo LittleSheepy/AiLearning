c8c205504dc3d1f8146122acdb575b93557da236
LittleSheepy <38182200+LittleSheepy@users.noreply.github.com>
2018-09-17 22:06:10 +0800

    添加一些打印
    添加一些打印

--- src/py3.x/ml/6.SVM/svm-simple.py
+++ src/py3.x/ml/6.SVM/svm-simple.py
@@ -8,7 +8,8 @@ Chapter 5 source file for Machine Learing in Action
 @author: Peter/geekidentity/片刻
 《机器学习实战》更新地址：https://github.com/apachecn/AiLearning
 """
-from numpy import *
+import numpy as np
+#from numpy import *
 import matplotlib.pyplot as plt
 
 
@@ -42,7 +43,7 @@ def selectJrand(i, m):
     """
     j = i
     while j == i:
-        j = int(random.uniform(0, m))
+        j = int(np.random.uniform(0, m))
     return j
 
 
@@ -77,14 +78,15 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
         b       模型的常量值
         alphas  拉格朗日乘子
     """
-    dataMatrix = mat(dataMatIn)
+    dataMatrix = np.mat(dataMatIn)
     # 矩阵转置 和 .T 一样的功能
-    labelMat = mat(classLabels).transpose()
-    m, n = shape(dataMatrix)
+    classMatrix = np.mat(classLabels)
+    labelMat = classMatrix.transpose()
+    m, n = np.shape(dataMatrix)
 
     # 初始化 b和alphas(alpha有点类似权重值。)
     b = 0
-    alphas = mat(zeros((m, 1)))
+    alphas = np.mat(np.zeros((m, 1)))
 
     # 没有任何alpha改变的情况下遍历数据的次数
     iter = 0
@@ -95,13 +97,17 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
         # 记录alpha是否已经进行优化，每次循环时设为0，然后再对整个集合顺序遍历
         alphaPairsChanged = 0
         for i in range(m):
-            # print('alphas=', alphas)
-            # print('labelMat=', labelMat)
-            # print('multiply(alphas, labelMat)=', multiply(alphas, labelMat))
+            #print("\n", i)
+            #print("dataMatrix[i, :] = ", dataMatrix[i, :])
+            #print('alphas=', alphas)
+            #print('labelMat=', labelMat)
+            #print('multiply(alphas, labelMat)=', np.multiply(alphas, labelMat))
+
             # 我们预测的类别 y = w^Tx[i]+b; 其中因为 w = Σ(1~n) a[n]*lable[n]*x[n]
-            fXi = float(multiply(alphas, labelMat).T*(dataMatrix*dataMatrix[i, :].T)) + b
+            fXi = float(np.multiply(alphas, labelMat).T*(dataMatrix*dataMatrix[i, :].T)) + b
             # 预测结果与真实结果比对，计算误差Ei
-            Ei = fXi - float(labelMat[i])
+            lableMat_i = float(labelMat[i])
+            Ei = fXi - lableMat_i
 
             # 约束条件 (KKT条件是解决最优化问题的时用到的一种方法。我们这里提到的最优化问题通常是指对于给定的某一函数，求其在指定作用域上的全局最小值)
             # 0<=alphas[i]<=C，但由于0和C是边界值，我们无法进行优化，因为需要增加一个alphas和降低一个alphas。
@@ -117,7 +123,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                 # 如果满足优化的条件，我们就随机选取非i的一个点，进行优化比较
                 j = selectJrand(i, m)
                 # 预测j的结果
-                fXj = float(multiply(alphas, labelMat).T*(dataMatrix*dataMatrix[j, :].T)) + b
+                fXj = float(np.multiply(alphas, labelMat).T*(dataMatrix*dataMatrix[j, :].T)) + b
                 Ej = fXj - float(labelMat[j])
                 alphaIold = alphas[i].copy()
                 alphaJold = alphas[j].copy()
@@ -132,7 +138,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                     H = min(C, alphas[j] + alphas[i])
                 # 如果相同，就没发优化了
                 if L == H:
-                    print("L==H")
+                    #print("L==H")
                     continue
 
                 # eta是alphas[j]的最优修改量，如果eta==0，需要退出for循环的当前迭代过程
@@ -143,12 +149,14 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                     continue
 
                 # 计算出一个新的alphas[j]值
-                alphas[j] -= labelMat[j]*(Ei - Ej)/eta
+                new_eta = labelMat[j]*(Ei - Ej)/eta
+                alphas[j] -= new_eta
                 # 并使用辅助函数，以及L和H对其进行调整
-                alphas[j] = clipAlpha(alphas[j], H, L)
+                new_alphas = clipAlpha(alphas[j], H, L)
+                alphas[j] = new_alphas
                 # 检查alpha[j]是否只是轻微的改变，如果是的话，就退出for循环。
                 if (abs(alphas[j] - alphaJold) < 0.00001):
-                    print("j not moving enough")
+                    #print("j not moving enough")
                     continue
                 # 然后alphas[i]和alphas[j]同样进行改变，虽然改变的大小一样，但是改变的方向正好相反
                 alphas[i] += labelMat[j]*labelMat[i]*(alphaJold - alphas[j])
@@ -167,7 +175,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                 alphaPairsChanged += 1
                 print("iter: %d i:%d, pairs changed %d" % (iter, i, alphaPairsChanged))
         # 在for循环外，检查alpha值是否做了更新，如果在更新则将iter设为0后继续运行程序
-        # 知道更新完毕后，iter次循环无变化，才推出循环。
+        # 直到更新完毕后，iter次循环无变化，才推出循环。
         if (alphaPairsChanged == 0):
             iter += 1
         else:
@@ -187,12 +195,12 @@ def calcWs(alphas, dataArr, classLabels):
     Returns:
         wc  回归系数
     """
-    X = mat(dataArr)
-    labelMat = mat(classLabels).transpose()
-    m, n = shape(X)
-    w = zeros((n, 1))
+    X = np.mat(dataArr)
+    labelMat = np.mat(classLabels).transpose()
+    m, n = np.shape(X)
+    w = np.zeros((n, 1))
     for i in range(m):
-        w += multiply(alphas[i] * labelMat[i], X[i, :].T)
+        w += np.multiply(alphas[i] * labelMat[i], X[i, :].T)
     return w
 
 
@@ -204,11 +212,11 @@ def plotfig_SVM(xMat, yMat, ws, b, alphas):
        http://blog.csdn.net/kkxgx/article/details/6951959
     """
 
-    xMat = mat(xMat)
-    yMat = mat(yMat)
+    xMat = np.mat(xMat)
+    yMat = np.mat(yMat)
 
     # b原来是矩阵，先转为数组类型后其数组大小为（1,1），所以后面加[0]，变为(1,)
-    b = array(b)[0]
+    b = np.array(b)[0]
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
@@ -216,13 +224,13 @@ def plotfig_SVM(xMat, yMat, ws, b, alphas):
     ax.scatter(xMat[:, 0].flatten().A[0], xMat[:, 1].flatten().A[0])
 
     # x最大值，最小值根据原数据集dataArr[:, 0]的大小而定
-    x = arange(-1.0, 10.0, 0.1)
+    x = np.arange(-1.0, 10.0, 0.1)
 
     # 根据x.w + b = 0 得到，其式子展开为w0.x1 + w1.x2 + b = 0, x2就是y值
     y = (-b-ws[0, 0]*x)/ws[1, 0]
     ax.plot(x, y)
 
-    for i in range(shape(yMat[0, :])[1]):
+    for i in range(np.shape(yMat[0, :])[1]):
         if yMat[0, i] > 0:
             ax.plot(xMat[i, 0], xMat[i, 1], 'cx')
         else:
@@ -242,13 +250,14 @@ if __name__ == "__main__":
 
     # b是常量值， alphas是拉格朗日乘子
     b, alphas = smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
-    print('/n/n/n')
+    print('\n')
     print('b=', b)
     print('alphas[alphas>0]=', alphas[alphas > 0])
-    print('shape(alphas[alphas > 0])=', shape(alphas[alphas > 0]))
+    print('shape(alphas[alphas > 0])=', np.shape(alphas[alphas > 0]))
     for i in range(100):
         if alphas[i] > 0:
             print(dataArr[i], labelArr[i])
     # 画图
     ws = calcWs(alphas, dataArr, labelArr)
     plotfig_SVM(dataArr, labelArr, ws, b, alphas)
+
